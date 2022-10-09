ec341071f4d200b57961b26a81510c109747066d
entho888 <djxdrayn@gmail.com>
2022-10-09 21:19:21 +0000

    Added uniform function

--- scripts/simulation.py
+++ scripts/simulation.py
@@ -5,18 +5,22 @@ import matplotlib.pyplot as plt
 
 rng = np.random.default_rng()
 
-def uniform(a,b,N) :
-    """uniform _summary_
-
-    _extended_summary_
+def uniform(low=float, high=float, size=None) :
+    """Return a sample of size "size" of uniform distribution over the interval [low, high)
 
     Args:
-        a (_type_): _description_
-        b (_type_): _description_
-        N (_type_): _description_
-    """
-    return "on verra"
+        low (float): lower bound of the interval the generated numbers are in
+        high (float): upper bound of the interval the generated numbers are in
+        size (tupple): output shape. if the given shape is (m*n*k) then m*n*k samples are generated. 
+            Default is None, in the case the function returns a single value.
 
+    Returns:
+        Array of random floats of shape size.
+    """
+    if low > high : 
+        print("Error from 'uniform' function of 'simulation' : lower bound of interval greater than upper bound")
+        return 
+    return (high - low)*rng.random(size=size) + low
 
 def standard_brownian_simulation(time_list, gaussian_list) :
     Brownian_list = [gaussian_list[0]]
@@ -28,3 +32,11 @@ def standard_brownian_simulation(time_list, gaussian_list) :
         Brownian_list.append(Brownian_list[i-1] + np.sqrt((t2-t1))*gaussian_list[i])
 
     return Brownian_list
+
+"""
+times = np.linspace(0,100,N)
+Brownian = standard_brownian_simulation(times, sample)
+plt.plot(times, Brownian)
+plt.show()
+"""
+# %%
