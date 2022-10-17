24a8316a15648843d5f5f4f0fc8ce9990b5ce6e7
entho888 <djxdrayn@gmail.com>
2022-10-17 23:30:27 +0000

    Prepared quizz3.py for the quizz

---
+++ quizz produits dérivées/quizz3.py
@@ -0,0 +1,9 @@
+### QUIZZ 2 : 18/10/2022 ###
+# Note : 
+
+# On importe les bonnes librairies
+import sys
+sys.path.append('/workspace/Derivatives/scripts')
+import derivatives as deriv
+import simulation as sim
+
