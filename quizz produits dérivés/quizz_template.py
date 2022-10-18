ba60b79ae0510ff17a28fad2731b0f0c706276b0
entho888 <djxdrayn@gmail.com>
2022-10-18 09:10:44 +0000

    Added quizz_template.py

---
+++ quizz produits dérivés/quizz_template.py
@@ -0,0 +1,92 @@
+### QUIZZ  X : XX/XX/202X ###
+# Note : XXX
+
+# On importe les bonnes librairies
+import sys
+sys.path.append('/workspace/M2-Proba-Finance/scripts')
+import derivatives as deriv
+import simulation as sim  
+import numpy as np
+
+##########################################################################################################################
+# TIPS : double clique sur les trucs écrits en latex du quizz ouvre un page où on peut selectionner ces trucs et les c/c.
+# Utile pour les données qui sont souvent en latex 
+##########################################################################################################################
+
+answers = open("/workspace/M2-Proba-Finance/quizz produits dérivés/quizz_X_answers.txt", "w")
+
+# Question 1
+
+
+print('Q1 answer : ', np.round(  1 , decimals = 1  ))
+answers.write('Q1 answer : ' + str( np.round( 1  , decimals = 1  )))
+
+# Question 2
+
+
+print('Q2 answer : ', np.round( 2  , decimals = 2 ))
+answers.write('\nQ2 answer : ' + str( np.round( 2  , decimals =  2 )))
+
+"""
+# Question 3
+
+
+print('Q3 answer : ', np.round(   , decimals =  ))
+answers.write('\Q3 answer : ' + str( np.round(   , decimals =   )))
+
+# Question 4
+
+
+print('Q4 answer : ', np.round(   , decimals =  ))
+answers.write('\nQ4 answer : ' + str( np.round(   , decimals =   )))
+
+# Question 5
+
+
+print('Q5 answer : ', np.round(   , decimals =  ))
+answers.write('\nQ5 answer : ' + str( np.round(   , decimals =   )))
+
+# Question 6
+
+
+print('Q6 answer : ', np.round(   , decimals =  ))
+answers.write('\nQ6 answer : ' + str( np.round(   , decimals =   )))
+
+# Question 7
+
+
+print('Q7 answer : ', np.round(   , decimals =  ))
+answers.write('\nQ7 answer : ' + str( np.round(   , decimals =   )))
+
+# Question 8
+
+
+print('Q8 answer : ', np.round(   , decimals =  ))
+answers.write('\nQ8 answer : ' + str( np.round(   , decimals =   )))
+
+# Question 9
+
+
+print('Q9 answer : ', np.round(   , decimals =  ))
+answers.write('\nQ9 answer : ' + str( np.round(   , decimals =   )))
+
+# Question 10
+
+
+print('Q10 answer : ', np.round(   , decimals =  ))
+answers.write('\nQ10 answer : ' + str( np.round(   , decimals =   )))
+
+# Question 11
+
+
+print('Q11 answer : ', np.round(   , decimals =  ))
+answers.write('\nQ11 answer : ' + str( np.round(   , decimals =   )))
+
+# Question 12
+
+
+print('Q12 answer : ', np.round(   , decimals =  ))
+answers.write('\nQ12 answer : ' + str( np.round(   , decimals =   )))
+"""
+
+answers.close()
